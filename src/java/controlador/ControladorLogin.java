9232a6c9f806d8e40e740a66597eca8d8e448714
CADELACRUZG <cadelacruzg@correo.url.edu.gt>
2026-05-14 08:05:52 -0600

    controlador login

---
+++ src/java/controlador/ControladorLogin.java
@@ -0,0 +1,113 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controlador;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ *
+ * @author chris
+ */
+@WebServlet(name = "ControladorLogin", urlPatterns = {"/ControladorLogin"})
+public class ControladorLogin extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String usuario = request.getParameter("usuario");
+        String password = request.getParameter("password");
+
+        HttpSession sesion = request.getSession();
+
+        if (usuario.equals("admin") && password.equals("123")) {
+
+            sesion.setAttribute("usuario", usuario);
+            sesion.setAttribute("rol", "admin");
+
+            response.sendRedirect("adminInicio.jsp");
+
+        } else if (usuario.equals("cliente") && password.equals("123")) {
+
+            sesion.setAttribute("usuario", usuario);
+            sesion.setAttribute("rol", "cliente");
+
+            response.sendRedirect("clientesInicio.jsp");
+
+        } else if (usuario.equals("repartidor") && password.equals("123")) {
+
+            sesion.setAttribute("usuario", usuario);
+            sesion.setAttribute("rol", "repartidor");
+
+            response.sendRedirect("repartidorInicio.jsp");
+
+        } else {
+
+            response.sendRedirect("login.jsp");
+
+        }
+
+    }
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ControladorLogin</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ControladorLogin at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
